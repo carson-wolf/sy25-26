244c4fb61e64070057ee4ccf94e400f9ba3992fa
carson <cjwol384@learner.dvusd.org>
2026-02-05 13:41:10 -0700

    quizz 3

--- quiz 2/quiz 2/quiz_2.py
+++ quiz 2/quiz 2/quiz_2.py
@@ -1,2 +1,25 @@
-for i in range(2,50,2):
-    print(i)
+w = int(input("enter the weight: "))
+if w < 100:
+    g = "small"
+elif 100 <= w < 200:
+    g = "medium"
+else:
+    g = "large"
+print(g)
+
+l = []
+for i in range (5):
+    bl = int(input("enter blemish"))
+    l.append(bl)
+tb = sum(l)
+av = tb/5
+print(f"total blemish count is {tb}")
+print(f"average blemish is {av}")
+
+pot = [0,2,5,1,0,8,3,0]
+p = []
+for i in pot:
+    if i == 0:
+        p.append(i)
+per = (len(p)/len(pot)) * 100
+print(per)
