437e66ce2b17275f8fb9dde262dae1c8baf4640f
carson <cjwol384@learner.dvusd.org>
2026-02-19 14:03:34 -0700

    assignment for feb 19 v2

--- list manager/list manager/list_manager.py
+++ list manager/list manager/list_manager.py
@@ -11,6 +11,7 @@ while True:
             dict[item] += quan
         else:
             dict[item] = quan
+        print("options: [1] add to list, [2] remove from list, [3] view list, [4] exit")
     elif choice == 2:
         item = input("enter the name of the item you are removing: ")
         quan = int(input(f"enter amount of {item} you want to remove: "))
@@ -21,3 +22,15 @@ while True:
                 dict[item] -= quan
         else:
             print(f"{item} is not in your list")
+        print("options: [1] add to list, [2] remove from list, [3] view list, [4] exit")
+    elif choice == 3:
+        for i in list(dict):
+            if dict[i] <= 0:
+                del dict[i]
+        print("your list: ")
+        for item in dict:
+            print(f"{item}: {dict[item]}")
+        print("options: [1] add to list, [2] remove from list, [3] view list, [4] exit")
+    else:
+        print("invalid choice, try again")
+        print("options: [1] add to list, [2] remove from list, [3] view list, [4] exit")
